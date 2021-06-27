05aac78720eceba0d768c960449fead6611d7069
Altair Pinto <eng.altairpinto@gmail.com>
2021-06-26 21:24:44 -0300

    User header components

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,33 +1,23 @@
-import React from 'react';
-import {
-  Text,
-  Image,
-  View,
-  TouchableOpacity,
-  TouchableOpacityProps
-} from 'react-native';
+import React from "react";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { Text, Image, View } from "react-native";
 
-import DiscordImage from '../../assets/discord.png'
+import DiscordImage from "../../assets/discord.png";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-type Props = TouchableOpacityProps & {
- title: string;
-}
+type Props = RectButtonProps & {
+  title: string;
+};
 
-export function ButtonIcon({title, ...rest }: Props){
-  return(
-    <TouchableOpacity 
-      style={styles.container}
-      { ...rest }
-    >
+export function ButtonIcon({ title, ...rest }: Props) {
+  return (
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImage} style={styles.icon} />
       </View>
 
-      <Text style={styles.title}>
-        {title}
-      </Text>
-    </TouchableOpacity>
-  )
+      <Text style={styles.title}>{title}</Text>
+    </RectButton>
+  );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Text, View, Image } from "react-native";
-import { ButtonIcon } from "../../components/ButtonIcon";
 
 import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { useNavigation } from "@react-navigation/native";
+
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate("Home");
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -24,7 +32,7 @@ export function SignIn() {
           favoritos com seus amigos
         </Text>
 
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+        <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
       </View>
     </View>
   );
